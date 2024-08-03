2293958e1db750f3b81b24a3be2449b298ae5736
Masumi Kawasaki <geeknees@gmail.com>
2024-08-03 23:58:39 +0900

    chore: Update npm dependencies for @astrojs/check, @astrojs/mdx, @astrojs/rss, @astrojs/sitemap, astro, astro-embed, and typescript
    Bump the versions of the following npm dependencies:
    - @astrojs/check from ^0.5.10 to ^0.9.1
    - @astrojs/mdx from ^2.2.3 to ^3.1.3
    - @astrojs/rss from ^4.0.4 to ^4.0.7
    - @astrojs/sitemap from ^3.0.5 to ^3.1.6
    - astro from ^4.5.9 to ^4.13.1
    - astro-embed from ^0.7.2 to ^0.7.2
    - typescript from ^5.3.3 to ^5.5.4
    
    This update includes bug fixes and performance improvements.

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,11 @@ import UnoCSS from "unocss/astro";
 import { THEME_CONFIG } from "./src/theme.config";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-
 import partytown from "@astrojs/partytown";
+
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,6 +20,15 @@ export default defineConfig({
       langs: [],
       wrap: true,
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [
+        rehypeKatex,
+        {
+          // Katexプラグインのオプション
+        },
+      ],
+    ],
   },
   integrations: [
     UnoCSS({
